8980f450e589b720679615c78f2183a4b608df37
GivenGlorious <givenglorious@gmail.com>
2026-08-07 23:12:56 +0700

    first commit

---
+++ SwapCase.py
@@ -0,0 +1,10 @@
+def swap_case(s):
+    if s == s.upper():
+        return s.lower()
+    else:
+        return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)

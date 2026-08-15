721d30a6982cc30608f63e0fb104dd22af8fe0c6
GivenGlorious <givenglorious@gmail.com>
2026-08-15 22:56:43 +0700

    first commit

--- The Captain's Room.py
+++ The Captain's Room.py
@@ -1,9 +1,11 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter #??????????????????? 
+
 k = int(input())
 rooms = list(map(int, input().split()))
 
-for room in rooms:
-    if rooms.count(room) == 1:
+count = Counter(rooms)
+
+for room in count:
+    if count[room] == 1:
         print(room)
         break
-   

649377d22f4efefe6df0a7ae2af121792448622c
GivenGlorious <givenglorious@gmail.com>
2026-08-19 20:34:16 +0700

    first commit

---
+++ Mod Divmod.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+m = int(input())
+divmods = divmod(n,m) 
+print(divmods[0])
+print(divmods[1])
+
+print(divmods)
